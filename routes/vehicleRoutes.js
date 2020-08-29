2cb52512bf40a73a5094cb327eb4111604128b47
GithungoIan <githungoian53@gmail.com>
2020-08-29 14:14:15 +0300

    vehicle routes

---
+++ routes/vehicleRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const vehicleController = require('../controllers/vehicleController');
+const router = express.Router();
+
+// Vehicle routes without id
+router
+	.route('/')
+	.get(vehicleController.getAllVehicles)
+	.post(vehicleController.postVehicle);
+
+	
+//  vehicle routes with id
+router
+	.route('/id')
+	.get(vehicleController.getVehicle)
+	.patch(vehicleController.updateVehicle)
+	.delete(vehicleController.updateVehicle);
