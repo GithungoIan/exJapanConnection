9a678c7a8b6ba66ba2ec82b5f387da078c93ffca
GithungoIan <githungoian53@gmail.com>
2020-08-29 13:01:38 +0300

    create vehicle model

---
+++ models/vehicleModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema(
+  {
+		name: {
+			type: String,
+			required: [true, 'A vehicle must have a name'],
+			trim: true,
+		},
+		slug: String,
+		spec: [
+			{
+				referenceNo: String,
+				millage: String,
+				modeCode: String,
+				registration: {
+					type: Date,
+					default: Date.now(),
+				},
+				manufacture: {
+					type: Date,
+					default: Date.now(),
+				},
+				modelGrade: String,
+				chassis: String,
+				engineSize: Number,
+				drive: String,
+				externalColor: String,
+				steering: String,
+				transmission: String,
+				fuel: String,
+				seats: Number,
+				doors: Number,
+			}
+		],
+		price: {
+			type: Number,
+			required:[true, 'A vehicle must have a price']
+		},
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, 'A vehicle must have a description']
+		},
+		description: {
+			type: String,
+			trim: true,
+		},
+		images: [String],
+		postedAt: {
+			type: Date,
+			default: Date.now(),
+			select: false
+		},
+		location: String,
+	}
+);
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
