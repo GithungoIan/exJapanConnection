5a54305703599885275982ad8cf12d5db27704e1
GithungoIan <githungoian53@gmail.com>
2020-08-29 14:58:18 +0300

    add routes to app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+
+
+const vehicleRouter = require('./routes/vehicleRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app  = express();
-
-
 // Middleware
 if(process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
 
+
+// routes
+app.use('/api/v1/vehicles', vehicleRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
