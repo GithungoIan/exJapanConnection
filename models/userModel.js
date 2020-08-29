19908d67b18acbebe1d3e4c3adc0565e68471e45
GithungoIan <githungoian53@gmail.com>
2020-08-29 15:25:50 +0300

    add virtual properties

--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,11 @@ const userSchema = new mongoose.Schema({
 		default: true,
 		select: false
 	},
-});
+},
+		{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  });
 
 
 const User = mongoose.model('User', userSchema);
