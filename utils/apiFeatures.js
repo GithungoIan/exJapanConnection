09688da99a8bddc9cf2835266528e36f1be1404d
GithungoIan <githungoian53@gmail.com>
2020-08-29 12:07:59 +0300

    implement paginate method

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -34,4 +34,14 @@ class APIFeatures {
 		}
 		return this;
 	}
+	
+	paginate() {
+		const page = this.queryString * 1 || 1;
+		const limit = this.queryString * 1 || 100;
+		const skip = (page - 1) * limit
+		
+		this.query = this.query.skip(skip).limit(limit);
+		
+		return this;
+	}
 }
