eb861c4b612a5a6db869e40c1ebe78a5fbe178b7
GithungoIan <githungoian53@gmail.com>
2020-08-29 12:04:47 +0300

    implement limitFields method

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,20 +1,20 @@
 class APIFeatures {
-    constructor(query, queryString){
-        this.query = query;
-        this.queryString = queryString;
-    }
+	constructor(query, queryString){
+		this.query = query;
+		this.queryString = queryString;
+	}
     
-    filter(){
-        const queryObj = {...this.queryString};
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach((el) => delete queryObj[el]);
-        
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-        
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    }
+	filter(){
+		const queryObj = {...this.queryString};
+		const excludeFields = ['page', 'sort', 'limit', 'fields'];
+		excludeFields.forEach((el) => delete queryObj[el]);
+		
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+		
+		this.query = this.query.find(JSON.parse(queryStr));
+		return this;
+	}
     
 	sort(){
 		if(this.queryString.sort){
@@ -22,6 +22,15 @@ class APIFeatures {
 			this.query = this.query.sort(sortBy);	
 		}else {
 			this.query = this.query.sort('-dotePosted');
+		}
+	}
+	
+	limitFields() {
+		if(this.queryString.fields) {
+			const fileds = this.queryString.fields.split(',').join(' ');
+			this.query = this.query.select(fileds);
+		}else {
+			this.query = this.query.select('-__v');
 		}
 	}
 }
