89c148ab8a5cd5d0053a2dda3b529d3cedc0f358
GithungoIan <githungoian53@gmail.com>
2020-10-03 19:59:20 +0300

    send email to user:

---
+++ utils/email.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async(options) => {
+  // 1) Create transport
+
+  // 2) Define the mail options
+
+  // 3) Actually send the mail
+}
+
+module.exports = sendEmail;
