f163f9766cec6c436bff39025e8a6283f11cb438
GithungoIan <githungoian53@gmail.com>
2020-10-03 21:52:24 +0300

    check if user changed password

--- models/userModel.js
+++ models/userModel.js
@@ -60,6 +60,14 @@ userSchema.pre('save', async function(next){
   // delete the passwordConfirm
   this.passwordConfirm = undefined;
   next();
+});
+
+userSchema.pre('save', function(next){
+  if(!this.isModified('password') || this.isNew){
+    return next();
+  };
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
 })
 
 
