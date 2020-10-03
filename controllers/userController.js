b3553fa9acb8217ffd9300add9d9241bdb38a3cc
GithungoIan <githungoian53@gmail.com>
2020-10-03 23:23:27 +0300

    refactor user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -117,37 +117,3 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
   next();
 });
-
-
-
-// get single user
-exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-}
-
-//  create new user account
-exports.createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    });
-}
-
-//  Update the user information
-exports.updateUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!'
-	});
-}
-
-// delete user from collection
-exports.deleteUser = (req, res) => {
-	res.status(500).json({
-		status: 'error',
-		message: 'This route is not yet defined!'
-});
-}
