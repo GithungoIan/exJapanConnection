f637bd7949c6940e49280178fc967b463d2097ff
GithungoIan <githungoian53@gmail.com>
2020-10-03 23:10:29 +0300

    Update current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,6 +54,18 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 
 exports.uploadUserPhoto = upload.single('photo');
 
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
+
+
 exports.getAllUsers = CatchAsync(async(req, res) => {
 
   const users = await User.find();
@@ -62,6 +74,33 @@ exports.getAllUsers = CatchAsync(async(req, res) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+})
+
+exports.updateMe = CatchAsync(async(req, res, next) => {
+  // 1) Create an error if user tries to update password
+  if (req.body.password || req.body.confirmPassword) {
+    return next(
+      new AppError(
+        'This route is not for password update please use /updateMyPassword'
+      )
+    );
+  }
+
+  // 2) Filter out unwanted data
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if(req.file) filteredBody.photo = req.file.filename;
+
+  // 3) Update THE USER Document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
     },
   });
 })
