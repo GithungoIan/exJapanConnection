0a0dad5d926b4d7b3fc4de8c03bd27bbfd0d0ce6
GithungoIan <githungoian53@gmail.com>
2020-10-03 18:31:49 +0300

    Upload vehicle photo

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,5 +1,37 @@
+const multer  = require('multer');
+const sharp = require('sharp');
 const Vehicle = require('../models/vehicleModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/AppError');
+const AppError = require('../utils/appError');
+
+
+
+//multer configyrations
+const multerStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'public/images/vehicles');
+	},
+	filename: (req, file, cb) => {
+		const ext = file.mimetype.split('/')[1];
+		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E7);
+		cb(null, `car-${uniqueSuffix}.${ext}`);
+	}
+});
+const multerFilter = (req, file, cb) => {
+	if(file.mimetype.startsWith('image')){
+		cb(null, true);
+	}else {
+		cb(new AppError('Not an image please upload only images', 404), false)
+	}
+};
+
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: multerFIlter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 //Get all vehicles available in the database
 exports.getAllVehicles = async (req, res) => {
@@ -10,9 +42,9 @@ exports.getAllVehicles = async (req, res) => {
 			.sort()
 			.limitFields().
 			paginate();
-			
+
 			const vehicles = await features.query;
-			
+
 			//Send response back
 			res.status(200).json({
 				status:'success',
@@ -33,20 +65,20 @@ exports.getAllVehicles = async (req, res) => {
 exports.getVehicle = async (req, res) => {
 	try {
 		const singleVehicle = await Vehicle.findById(req.params.id);
-		
+
 		res.status(200).json({
 			status: 'success',
 			data: {
 				singleVehicle
 			}
 		})
-		
+
 	} catch (err) {
 		res.status(404).json({
 			status:'fail',
 			message: err.messages
 		})
-		
+
 	}
 }
 
@@ -54,7 +86,7 @@ exports.getVehicle = async (req, res) => {
 exports.postVehicle = async (req, res) => {
 	try {
 		const newVehicle = await Vehicle.create(req.body);
-		
+
 		res.status(201).json({
 			status:'success',
 			data:{
@@ -73,14 +105,14 @@ exports.postVehicle = async (req, res) => {
 exports.updateVehicle = async (req, res) => {
 	try {
 		const vehicle = await Vehicle.findByIdAndUpdate(req.params.id, req.body);
-		
+
 		res.status(200).json({
 			status: 'success',
 			data: {
 				vehicle
 			}
 		});
-		
+
 	} catch (err) {
 		res.status(404).json({
 			status:'fail',
@@ -93,7 +125,7 @@ exports.updateVehicle = async (req, res) => {
 exports.deleteVehicle = async (req, res) => {
 	try {
 		await Vehicle.findByIdAndDelete(req.params.id);
-		
+
 		res.status(204).json({
 			status:'success',
 			data: null
