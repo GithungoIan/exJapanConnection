d79a27fc9c3d91eb5a97aff09197e0a4644e48a2
GithungoIan <githungoian53@gmail.com>
2020-10-03 20:12:37 +0300

    Handle error on development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,28 @@
 const AppError = require('../utils/appError');
 
+const sendErrorDev = (err, req, res) => {
+  // API
+  if(req.originalUrl.startsWith('/api')){
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  }
+
+  // Rendered Website
+  console.error('ERROR 🔥 ', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went Wrong',
+    msg: err.message
+  });
+}
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  
+  if(process.env.Node_Env === 'development'){
+    sendErrorDev(err, req, res);
+  }
 }
