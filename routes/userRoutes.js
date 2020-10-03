1a82209c1b37a78c7ca32d7809595f00ac5ac71f
GithungoIan <githungoian53@gmail.com>
2020-10-03 22:46:31 +0300

    reset password route route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,7 @@ router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
+router.post('/resetPassword/:token', authController.resetPassword);
 
 router
 	.route('/')
