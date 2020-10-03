a745eaf9f8e4ad3ae08e4139943cbc291ec4bd6a
GithungoIan <githungoian53@gmail.com>
2020-10-03 20:06:44 +0300

    error handling controller

---
+++ controllers/errorController.js
@@ -0,0 +1,8 @@
+const AppError = require('../utils/appError');
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  
+}
