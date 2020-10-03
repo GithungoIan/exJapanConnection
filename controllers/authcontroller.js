610d2c1d311267de647869e073f41e4f68b1eb57
GithungoIan <githungoian53@gmail.com>
2020-10-03 22:34:26 +0300

    reset user password

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const {promisify} = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -200,4 +201,33 @@ expoers.forgotPassword = catchAsync(async (req, res, next) => {
     )
   );
   }
+});
+
+// reset password
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get User based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) If token has not expired and there is a user, set password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3) Update changedPasswordsAt property for the user
+
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res);
 })
