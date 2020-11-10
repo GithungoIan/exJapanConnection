2209c71b7320d1b3f98dd2eb568f7b9473012229
GithungoIan <githungoian53@gmail.com>
2020-11-10 14:56:39 +0300

    Updated vehicle model

--- models/vehicleModel.js
+++ models/vehicleModel.js
@@ -1,77 +1,64 @@
+
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const vehicleSchema = new mongoose.Schema(
-  {
-		name: {
-			type: String,
-			required: [true, 'A vehicle must have a name'],
-			trim: true,
-		},
-		slug: String,
-    overview: [
-      {
-        millage: Number,
-        condition: String,
-        BodyType: String,
-        color: String,
-        Fuel: String,
-        transmission: String,
-        drive: String,
-        DutyType: String,
-        InteriorType: String,
-        engineSize: Number,
-        year:Number,
-        referenceNo: String,
-        registration: {
-          type: Date,
-          default: Date.now(),
-        },
-        model: String
-      }
-    ],
-    Features: [String],
-		price: {
-			type: Number,
-			required:[true, 'A vehicle must have a price']
-		},
-		summary: {
-			type: String,
-			trim: true,
-			required: [true, 'A vehicle must have a description']
-		},
-		description: {
-			type: String,
-			trim: true,
-		},
-    imageCover: {
-      type: String,
-      required: [true, 'A vehicle must have a cover image']
-    },
-		images: [String],
-		postedAt: {
-			type: Date,
-			default: Date.now(),
-			select: false
-		},
-		location: String,
-	},
-		{
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
+const vehicleSchema = new mongoose.Schema({
+  make: {
+    type: String,
+    required: [true, 'A vehicle must have a make'],
+  },
+  slug: String,
+  model: {
+    type: String,
+    required: [true, 'A vehicle must have a model'],
+  },
+  price: {
+    type: Number,
+  },
+  instock: {
+    type: Boolean,
+    default: true,
+  },
+  overview: [
+    {
+      millage: Number,
+      condition: String,
+      bodyType: String,
+      color: String,
+      Fuel: String,
+      transmission: String,
+      drive: String,
+      dutyType: String,
+      InteriorType: String,
+      engineSize: Number,
+      year: Number,
+      referenceNo: String
+    }
+  ],
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A vehicle must have a cover image'],
+  },
+  images: [String],
+  location: String
+},
+{
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
+}
 );
-// QUERY Middleware: runs before the save() and create()
-vehicleSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true });
+
+// QUERY MIDDLEWARE: RUN  BEFORE SAVE() AND CREATE()
+vehicleSchema.pre('save', function(next) {
+  this.slug = Slugify(this.model, {lower: true});
   next();
 });
 
-vehicleSchema.post(/^find/, function (docs, next) {
-  // console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  // console.log(docs);
-  next();
-});
 
-const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+
+const Vehicle = mongoose.model('vehicle', vehicleSchema);
 module.exports = Vehicle;
