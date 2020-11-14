f117827673825631079acef5860698ac6b72d171
GithungoIan <githungoian53@gmail.com>
2020-11-14 17:27:30 +0300

    log error stack trace

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
+  console.log(err.stack);
   process.exit(1);
 });
 
