72a141615f400d111cc6f854a7f7001d1034245f
GithungoIan <githungoian53@gmail.com>
2020-11-14 17:31:15 +0300

    fix error

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -3,28 +3,28 @@ class APIFeatures {
 		this.query = query;
 		this.queryString = queryString;
 	}
-    
+
 	filter(){
 		const queryObj = {...this.queryString};
 		const excludeFields = ['page', 'sort', 'limit', 'fields'];
 		excludeFields.forEach((el) => delete queryObj[el]);
-		
+
 		let queryStr = JSON.stringify(queryObj);
 		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-		
+
 		this.query = this.query.find(JSON.parse(queryStr));
 		return this;
 	}
-    
+
 	sort(){
 		if(this.queryString.sort){
 			const sortBy = this.queryString.sort.split(',').join(' ');
-			this.query = this.query.sort(sortBy);	
+			this.query = this.query.sort(sortBy);
 		}else {
-			this.query = this.query.sort('-dotePosted');
+			this.query = this.query.sort('-datePosted');
 		}
 	}
-	
+
 	limitFields() {
 		if(this.queryString.fields) {
 			const fileds = this.queryString.fields.split(',').join(' ');
@@ -34,14 +34,14 @@ class APIFeatures {
 		}
 		return this;
 	}
-	
+
 	paginate() {
 		const page = this.queryString * 1 || 1;
 		const limit = this.queryString * 1 || 100;
 		const skip = (page - 1) * limit
-		
+
 		this.query = this.query.skip(skip).limit(limit);
-		
+
 		return this;
 	}
 }
