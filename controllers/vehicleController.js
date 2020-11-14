f510121857d11f1086169fbdf0df79baba12da85
GithungoIan <githungoian53@gmail.com>
2020-11-14 15:28:24 +0300

    upload and resize vehicle image

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -24,48 +24,61 @@ const multerStorage = multer.diskStorage({
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-	if(file.mimetype.startsWith('image')){
-		cb(null, true);
-	}else {
-		cb(new AppError('Not an image please upload only images', 404), false)
-	}
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image please upload only images', 404), false);
+  }
 };
 
 const upload = multer({
-	storage: multerStorage,
-	fileFilter: multerFilter,
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
 
-exports.uploadTourImages = upload.fields([
+exports.uploadVehicleImages = upload.fields([
 	{name: 'imageCover', maxCount: 1},
 	{name: 'images', maxCount: 7}
 ]);
 
 // Resize vehicle photo
-exports.resizeVehiclePhoto = catchAsync(async(req, res, next) => {
-	if(!req.file){
-		return next()
-	}
-	const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E7);
-	req.file.filename = `car-${uniqueSuffix}.jpeg`;
-
-	sharp(req.file.buffer)
-		.resize(400, 500)
+exports.resizeVehicleImages = catchAsync(async(req, res, next) => {
+	if(!req.files.imageCover || !req.files.images) return next();
+	
+	// 1) Cover Image
+	req.body.imageCover = 'vehicle-${req.params.id}-${Date.now()}-cover.jpeg';
+	await sharp(req.files.imageCover[0].buffer)
+		.resize(400, 600)
 		.toFormat('jpeg')
 		.jpeg({quality: 90})
-		.toFile(`public/images/vehicles/${req.file.filename}`);
-
-	next()
+		.toFile(`public/images/vehicle/${filename}`);
+		
+		// 2) Images 
+		req.body.images = [];
+		await Promise.all(
+			req.files.images.map(async (file, i) => {
+				const filename = `vehicle-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+				
+				await sharp (file.buffer)
+					.resize(700, 5000)
+					.toFormat('jpeg')
+					.jpeg({quality: 90})
+					.toFile(`public/images/vehicles/${filename}`);
+			})
+		);
+		next();
 });
+
+
 //Get all vehicles available in the database
 exports.getAllVehicles = catchAsync(async(req, res, next) => {
 	// Execute query
 	const features = new APIFeatures(Vehicle.find(), req.query)
 		.filter()
 		.sort()
-		.limitFields().
-		paginate();
+		.limitFields()
+		.paginate();
 
 	const vehicles = await features.query;
 
@@ -80,7 +93,7 @@ exports.getAllVehicles = catchAsync(async(req, res, next) => {
 })
 
 // Get single vehicle based on its identifier
-exports.getVehicle =catchAsync(async(req, res, next) => {
+exports.getVehicle = catchAsync(async(req, res, next) => {
 	const vehicle = await Vehicle.findById(req.params.id);
 
 	if(!vehicle){
