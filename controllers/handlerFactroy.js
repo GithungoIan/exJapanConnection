bf58d0f5d6a92d084a7072e15464dba3e22372cb
GithungoIan <githungoian53@gmail.com>
2020-11-14 15:42:09 +0300

    create one handler

--- controllers/handlerFactroy.js
+++ controllers/handlerFactroy.js
@@ -38,3 +38,16 @@ exports.updateOne = (Model) => {
     
   });
 }
+
+exports.createOne = (Model) => {
+  catchAsync(async(req, res, next) => {
+    const doc = await Model.create(req.body);
+    
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+}
