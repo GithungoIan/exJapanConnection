a02c8ad42595e201240a8b1a07c0395f4be378f2
GithungoIan <githungoian53@gmail.com>
2021-01-16 10:31:37 +0300

    upload and resize vehicle images

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -1,23 +1,23 @@
 const multer  = require('multer');
-// const sharp = require('sharp');
+const sharp = require('sharp');
 const Vehicle = require('../models/vehicleModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactroy');
 const AppError = require('../utils/appError');
 
 
-// const multerStorage = multer.memoryStorage();
+const multerStorage = multer.memoryStorage();
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/vehicles');
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix =  Math.round(Math.random() * 1E5);
-    const ext = file.mimetype.split('/')[1];
-    cb (null, `vehicle-cover-${uniqueSuffix}-${Date.now()}.${ext}`);
-  }
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/images/vehicles');
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix =  Math.round(Math.random() * 1E5);
+//     const ext = file.mimetype.split('/')[1];
+//     cb (null, `vehicle-cover-${uniqueSuffix}-${Date.now()}.${ext}`);
+//   }
+// });
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -32,42 +32,45 @@ const upload = multer({
   fileFilter: multerFilter
 });
 
-exports.uploadVehicleImage = upload.single('imageCover');
+// exports.uploadVehicleImage = upload.single('imageCover');
+exports.uploadVehicleImages = upload.fields([
+  {name: 'imageCover', maxCount: 1},
+  {name: 'images', maxCount: 8}
+]);
 
-// exports.uploadVehicleImages = upload.fields([
-// 	{name: 'imageCover', maxCount: 1},
-// 	{name: 'images', maxCount: 7}
-// ]);
+exports.resizeVehicleImages = catchAsync(async (req, res, next) => {
+  if(!req.files.imageCover || !req.files.images) return next();
 
-// Resize vehicle photo
-// exports.resizeVehicleImages = catchAsync(async(req, res, next) => {
-// 	if(!req.files.imageCover || !req.files.images) return next();
-//
-// 	// 1) Cover Image
-// 	req.body.imageCover = `vehicle-${Date.now()}-cover.jpeg`
-// 	await sharp(req.files.imageCover[0].buffer)
-// 		.resize(400, 600)
-// 		.toFormat('jpeg')
-// 		.jpeg({quality: 90})
-// 		.toFile(`public/images/vehicles/${req.body.imageCover}`);
-//
-// 		// 2) Images
-// 		req.body.images = [];
-// 		await Promise.all(
-// 			req.files.images.map(async (file, i) => {
-// 				const filename = `vehicle-${Date.now()}-${i + 1}.jpeg`;
-//
-// 				await sharp (file.buffer)
-// 					.resize(700, 500)
-// 					.toFormat('jpeg')
-// 					.jpeg({quality: 90})
-// 					.toFile(`public/images/vehicles/${filename}`);
-//
-// 				req.body.images.push(filename);
-// 			})
-// 		);
-// 		next();
-// });
+  // cover image
+  const uniqueSuffix =  Math.round(Math.random() * 1E5);
+  req.body.imageCover = `vehicle-${uniqueSuffix}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(900, 700)
+    .toFormat('jpeg')
+    .jpeg({quality: 90})
+    .toFile(`public/images/vehicles/${req.body.imageCover}`);
+
+
+  // Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const uniqueSuffix =  Math.round(Math.random() * 1E5);
+      const filename = `vehicle-${uniqueSuffix}-${Date.now()}-${i + 1}.jpeg`;
+
+
+      await sharp(file.buffer)
+        .resize(900, 700)
+        .toFormat('jpeg')
+        .jpeg({quality: 90})
+        .toFile(`public/images/vehicles/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+  next();
+});
 
 
 exports.getAllVehicles = factory.getAll(Vehicle);
