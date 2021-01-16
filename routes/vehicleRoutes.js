8047285dde13d6d63cb588673f0870dc87b2514e
GithungoIan <githungoian53@gmail.com>
2021-01-16 09:40:57 +0300

    update vehicle cover image upload route

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -6,14 +6,13 @@ router.
 	route('/')
 	.get(vehicleController.getAllVehicles)
 	.post(vehicleController.postVehicle);
-	
+
 router
 	.route('/:id')
 	.get(vehicleController.getVehicle)
 	.patch(
-		vehicleController.uploadVehicleImages,
-		vehicleController.resizeVehicleImages,
+		vehicleController.uploadVehicleImage,
 		vehicleController.updateVehicle)
 	.delete(vehicleController.deleteVehicle)
-	
+
 module.exports = router
