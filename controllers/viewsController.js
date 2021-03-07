7bf3342fc8dc7d47be8f202f2236d9158e335c1b
GithungoIan <githungoian53@gmail.com>
2021-03-07 12:44:23 +0300

    Added new Views

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Vehicle = require('../models/vehicleModel');
+const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -17,20 +18,93 @@ exports.getOverview =  catchAsync(async(req, res, next) => {
 exports.getVehicle = catchAsync(async (req, res, next) => {
   // 1) get the data, for the requested tour
   const vehicle = await Vehicle.findOne({slug: req.params.slug});
-  // 2) Build Template
-  // 3) Render template using data from 1)
+
+  if(!vehicle) {
+    return next(new AppError('There is no Vehicle with that name.', 404));
+  }
+
   res.status(200).render('vehicle', {
-    title: 'Mazda Demio',
+    title: `${vehicle.slug}`,
     vehicle
   });
 });
 
-
 exports.landing = catchAsync(async(req, res, next) => {
-  // const vehicles = await Vehicle.find({featuredVehicle: {$ne: false}})
-  // console.log(vehicles);
+  const featuredVehicles = await Vehicle.find({featuredVehicle: {$ne: false}});
+
   res.status(200).render('landing',{
     title: 'ExJpLimited',
-    // vehicles
+    featuredVehicles
   })
+});
+
+
+exports.getAbout = (req, res) => {
+  res.status(200).render('about', {
+    title: 'About us'
+  });
+};
+
+exports.getContactUs = (req, res) => {
+  res.status(200).render('contact', {
+    title: 'Contact us'
+  });
+};
+
+
+exports.getTaxInfo = (req, res) => {
+  res.status(200).render('taxInfo', {
+    title: 'Tax Information'
+  });
+};
+
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
+};
+
+
+exports.getSignUpForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Sign Up for your account'
+  });
+}
+
+exports.getResetPasswordForm = (req, res) => {
+  res.status(200).render('forgotPassword', {
+    title: 'Reset Password'
+  });
+}
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
+  });
+};
+
+exports.getBlog = (req, res) => {
+  res.status(200).render('blog', {
+    title: 'Blog'
+  });
+}
+
+
+exports.updateUserData = catchAsync(async(req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+  req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser
+  });
 });
