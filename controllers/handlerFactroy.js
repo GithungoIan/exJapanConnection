fe74f1adf24d97f19eb6060bebce0d1a62e3a5eb
GithungoIan <githungoian53@gmail.com>
2021-03-07 12:01:05 +0300

    Updated factory hander

--- controllers/handlerFactroy.js
+++ controllers/handlerFactroy.js
@@ -1,10 +1,9 @@
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
-const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
-
-exports.deleteOne = (Model) =>
-  catchAsync(async(req, res, next) => {
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
 
     if (!doc) {
@@ -17,15 +16,14 @@ exports.deleteOne = (Model) =>
     });
   });
 
-
-exports.updateOne = (Model) =>
-  catchAsync(async(req, res, next) => {
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     });
 
-    if(!doc){
+    if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
 
@@ -35,12 +33,10 @@ exports.updateOne = (Model) =>
         data: doc
       }
     });
-
   });
 
-
-exports.createOne = (Model) =>
-  catchAsync(async(req, res, next) => {
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
 
     res.status(201).json({
@@ -51,15 +47,16 @@ exports.createOne = (Model) =>
     });
   });
 
-
-exports.getOne = (Model) =>
-  catchAsync(async(req, res, next) => {
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
     let query = Model.findById(req.params.id);
-    const doc = await query
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
 
-    if(!doc){
-      return next(new AppError('No document found with that Id', 404));
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
     }
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -68,30 +65,26 @@ exports.getOne = (Model) =>
     });
   });
 
-
-exports.getAll = (Model) =>
-  catchAsync(async(req, res, next) => {
-    //To allow for nested Get reviews on vehicles(simple hack)
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (hack)
     let filter = {};
-    if(req.params.vehicleId) {
-      filter = {vehicle: req.params.vehicleId};
-    }
+    if (req.params.tourId) filter = { tour: req.params.tourId };
 
     const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
       .paginate();
-
     // const doc = await features.query.explain();
     const doc = await features.query;
 
-    // Send Response
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
-      result: doc.length,
+      results: doc.length,
       data: {
-        doc
+        data: doc
       }
     });
   });
