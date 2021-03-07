2547ff05015f0566c0fc2efd77244ab1edc8b32f
GithungoIan <githungoian53@gmail.com>
2021-03-07 11:22:41 +0300

    update dev data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Vehicle = require('./../../models/vehicleModel');
 
-dotenv.config({path: './config.env'});
+dotenv.config({path: '../../config.env'});
 
 const DB = process.env.DATABASE_LOCAL;
 
