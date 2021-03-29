7d9ccd8c175e7986e6521d4ff3296644df25f1f9
GithungoIan <githungoian53@gmail.com>
2021-03-29 11:23:04 +0300

    added new view routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.get('/', viewsController.landing);
 router.get('/overview', viewsController.getOverview);
 router.get('/vehicle/:slug', viewsController.getVehicle);
+router.get('/about', viewsController.getAbout);
+router.get('/contact', viewsController.getContactUs);
 
 module.exports = router
