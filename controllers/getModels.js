2ee5bbd61e716ca68031b75f966107de5a4fac6e
GithungoIan <githungoian53@gmail.com>
2021-04-07 14:37:50 +0300

    query for models

--- controllers/getModels.js
+++ controllers/getModels.js
@@ -1,9 +1,19 @@
 const Vehicle = require('../models/vehicleModel');
 
-export async function getModels(){  
-  const makes = await Vehicle.aggregate([
-    { "$group": {_id: "$make", count: {$sum: 1}}}
+export async function getModels(make){
+  const models = await Vehicle.aggregate([
+    {
+      $match : {
+        make : `${make}`
+      }
+    },
+    {
+      "$group": {
+        _id: "$model",
+        count: {$sum: 1}
+      }
+    }
   ]);
-  console.log(makes);
-  return makes;
+  console.log(models);
+  // return makes;
 }
